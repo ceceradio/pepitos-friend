b715ba4a68cd772426f0f8ad5660d60cacc48020
= <charles.wagner@devfluid.com>
2015-01-18 04:00:35 +0000

    added behavior to tweet when pepito has stayed home or been out for awhile

--- main.js
+++ main.js
@@ -2,39 +2,76 @@
 var twitter = require('twitter');
 var fs = require('fs');
 var twit = new twitter(config.twitter);
+var moment = require('moment');
 
 var intervalLength = 10000;
-var since_id = 0;
+var saveData = {since_id: 0, lastNormalTweet: 0};
 
-if (fs.existsSync('since_id.txt')) {
-    var since_id_str = fs.readFileSync('since_id.txt', {encoding: 'utf8'});
-
-    if (typeof since_id_str !== undefined && since_id_str !== false && since_id_str != "")
-        since_id = since_id_str;
+if (fs.existsSync('saveData.json')) {
+    saveData = JSON.parse(fs.readFileSync('saveData.json', {encoding: 'utf8'}));
+    console.log("Loaded Data: ");
+    console.log(saveData);
 }
 function checkPepitosTweets() {
     var data = {screen_name: "pepitothecat", exclude_replies: true};
-    if (since_id > 0)
-        data.since_id = since_id;
+    if (saveData.since_id > 0)
+        data.since_id = saveData.since_id;
     twit.get('statuses/user_timeline', data, function(error, tweets, response){
         if(error) console.log(error);
-        if (tweets.length > 0 && tweets[0].id != since_id) {
-            var dateText = tweets[0].text.match(/\([\d]+:[\d]+:[\d]+\)/)[0];
-            var text=config.welcomehome;
-            if (tweets[0].text.indexOf("out") >= 0)
-                text=config.staysafe;
-            text += " "+dateText;
-            twit.post('statuses/update', {in_reply_to_status_id: tweets[0].id_str, status: "@PepitoTheCat "+text}, function(error, body, response) {
-                if(error) console.log(error);;
-            });
-            since_id = tweets[0].id_str;
-            console.log(tweets[0].text);  // The tweets.
-            console.log(tweets[0].id_str);
-            fs.writeFile('since_id.txt', since_id, {}, function(err) {
-                if (err) console.log(err);
-                else console.log("Saved since_id");
+        if (tweets.length > 0) {
+            var tweetData = getResponseTweet(tweets[0]);
+            doTweet(tweetData);
+        }
+        else if (new Date(saveData.lastNormalTweet).getTime() + config.normalTweetInterval < Date.now()) {
+            twit.get('statuses/show/'+saveData.since_id, data, function(error, tweet, response){
+                if(error) console.log(error);
+                var tweetData = getNormalTweet(tweet);
+                doTweet(tweetData);
             });
         }
+    });
+}
+function doTweet(tweetData) {
+    if (typeof tweetData.status === "undefined")
+        return;
+    twit.post('statuses/update', tweetData, function(error, body, response) {
+        if(error) console.log(error);;
+    });
+}
+function getNormalTweet(tweet) {
+    var response = {};
+    var now = new Date();
+    response.status=config.athome;
+    if (tweet.text.indexOf("out") >= 0)
+        response.status=config.outtoolong;
+    
+    response.status += " ("+moment().utc().format("HH:mm:ss")+")";
+    console.log("No activity: "+response.status);
+    saveData.lastNormalTweet = now.getTime();
+    SaveData();
+    return response;
+}
+function getResponseTweet(tweet) {
+    var response = {};
+    if (tweet.id_str != saveData.since_id) {
+        response.status=config.welcomehome;
+        if (tweet.text.indexOf("out") >= 0)
+            response.status=config.staysafe;
+            
+        response.status += " "+dateText;
+        response.in_reply_to_status_id = tweet.id_str;
+        saveData.since_id = tweet.id_str;
+        saveData.lastNormalTweet = new Date(tweet.created_at).getTime();
+        console.log(tweet.id_str + ": " + tweet.text);  // The tweets.
+        console.log("Response: "+response.status);
+        SaveData();
+    }
+    return response;
+}
+function SaveData() {
+    fs.writeFile('saveData.json', JSON.stringify(saveData), {}, function(err) {
+        if (err) console.log(err);
+        else console.log("Saved");
     });
 }
 checkPepitosTweets();
