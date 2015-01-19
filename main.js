19af7357a87abbeac9e7c5cfd59675bb3328407e
= <charles.wagner@devfluid.com>
2015-01-19 05:34:35 +0000

    added support for arrays of strings for messages, and returns from errors instead of just printing to console and breaking

--- main.js
+++ main.js
@@ -17,14 +17,14 @@ function checkPepitosTweets() {
     if (saveData.since_id > 0)
         data.since_id = saveData.since_id;
     twit.get('statuses/user_timeline', data, function(error, tweets, response){
-        if(error) console.log(error);
+        if(error) { console.log(error); return; }
         if (tweets.length > 0) {
             var tweetData = getResponseTweet(tweets[0]);
             doTweet(tweetData);
         }
         else if (new Date(saveData.lastNormalTweet).getTime() + config.normalTweetInterval < Date.now()) {
             twit.get('statuses/show/'+saveData.since_id, data, function(error, tweet, response){
-                if(error) console.log(error);
+                if(error) { console.log(error); return; }
                 var tweetData = getNormalTweet(tweet);
                 doTweet(tweetData);
             });
@@ -41,9 +41,16 @@ function doTweet(tweetData) {
 function getNormalTweet(tweet) {
     var response = {};
     var now = new Date();
-    response.status=config.athome;
-    if (tweet.text.indexOf("out") >= 0)
-        response.status=config.outtoolong;
+    if (typeof config.athome === "string")
+        response.status=config.athome;
+    else
+        response.status=config.athome[Math.floor(Math.random() * config.athome.length)];
+    if (tweet.text.indexOf("out") >= 0) {
+        if (typeof config.outtoolong === "string")
+            response.status=config.outtoolong;
+        else
+            response.status=config.outtoolong[Math.floor(Math.random() * config.outtoolong.length)];
+    }
     
     response.status += " ("+moment().zone("+0100").format("HH:mm:ss")+")";
     console.log("No activity: "+response.status);
@@ -54,9 +61,16 @@ function getNormalTweet(tweet) {
 function getResponseTweet(tweet) {
     var response = {};
     if (tweet.id_str != saveData.since_id) {
-        response.status=config.welcomehome;
-        if (tweet.text.indexOf("out") >= 0)
-            response.status=config.staysafe;
+        if (typeof config.welcomehome === "string")
+            response.status=config.welcomehome;
+        else
+            response.status=config.welcomehome[Math.floor(Math.random() * config.welcomehome.length)];
+        if (tweet.text.indexOf("out") >= 0) {
+            if (typeof config.staysafe === "string")
+                response.status=config.staysafe;
+            else
+                response.status=config.staysafe[Math.floor(Math.random() * config.staysafe.length)];
+        }
         var dateText = tweet.text.match(/\([\d]+:[\d]+:[\d]+\)/)[0];
         response.status = "@PepitoTheCat "+response.status+" "+dateText;
         response.in_reply_to_status_id = tweet.id_str;
